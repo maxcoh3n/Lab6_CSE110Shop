0b5e94a4b5073c7737a560757423a7e059b77964
maxcoh3n <maxcoh3n@gmail.com>
2021-02-11 18:39:41 -0800

    steps 1-3

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
 // Script.js
 
+
+const myStorage = window.localStorage;
+const productsString = localStorage.getItem('products');
+let productsArr = JSON.parse(productsString);
+console.log(productsArr[0])
+
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch('https://fakestoreapi.com/products')
+   .then(res => res.text() )
+   .then(res=>{
+    const myStorage = window.localStorage;
+    myStorage.setItem('products',res);
+   })
 });
+
+
+
+var node = document.createElement('product-item');
+node.addImg(productsArr[0].image, productsArr[0].title);
+node.addTitle(productsArr[0].title);
+node.addPrice(productsArr[0].price);
+
+// node.bleep(); 
+document.querySelector('ul').appendChild(node);
