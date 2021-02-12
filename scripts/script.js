47524cd2afe6073937f842fac42d8019006b9039
maxcoh3n <maxcoh3n@gmail.com>
2021-02-11 21:54:13 -0800

    move around stuff in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,25 @@
 // Script.js
 
 
-const myStorage = window.localStorage;
-const productsString = localStorage.getItem('products');
-let productsArr = JSON.parse(productsString);
-console.log(productsArr[0])
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.text())
+    .then((res) => {
+      const myStorage = window.localStorage;
+      myStorage.setItem("products", res);
+    });
 
-
-window.addEventListener('DOMContentLoaded', () => {
-  fetch('https://fakestoreapi.com/products')
-   .then(res => res.text() )
-   .then(res=>{
-    const myStorage = window.localStorage;
-    myStorage.setItem('products',res);
-   })
+  const myStorage = window.localStorage;
+  const productsString = localStorage.getItem("products");
+  let productsArr = JSON.parse(productsString);
+  for (let product of productsArr) {
+    var productItem = document.createElement("product-item");
+    productItem.addImg(product.image, productsArr[0].title);
+    productItem.addTitle(product.title);
+    productItem.addPrice(product.price);
+    document.querySelector("ul").appendChild(productItem);
+    break;
+  }
 });
 
-
-
-var node = document.createElement('product-item');
-node.addImg(productsArr[0].image, productsArr[0].title);
-node.addTitle(productsArr[0].title);
-node.addPrice(productsArr[0].price);
-
-// node.bleep(); 
-document.querySelector('ul').appendChild(node);
+// node.bleep();
